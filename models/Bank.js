7f0748c27ae14d845e74b447e050cd3413b1fbb2
leenguyenzz <email-cua-ban@gmail.com>
2026-05-13 06:21:09 +0700

    ver

--- models/Bank.js
+++ models/Bank.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const BankSchema = new mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     // accountNumber: { type: String, required: true, unique: true },
     // accountHolder: { type: String, required: true },
     balance: { type: Number, default: 0 }
