da6500cbc784ccbf2a1890fcb4453cbaaa1bd6bd
leenguyenzz <email-cua-ban@gmail.com>
2026-05-12 12:29:02 +0700

    v

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,9 +4,7 @@ const jwt = require('jsonwebtoken');
 exports.protect = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     console.log("Token nhận được ở server:", token); // Log để kiểm tra token có được gửi lên không
-    // 1. Log để xem headers thực tế
-    console.log("Headers nhận được:", req.headers);
-    // 2. Kiểm tra nếu header hoàn toàn không tồn tại
+    
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         console.log("Lỗi: Header Authorization trống hoặc sai định dạng Bearer");
@@ -17,6 +15,7 @@ exports.protect = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
+        console.log("Token đã được giải mã:", decoded); // Log thông tin user từ token
         next();
     } catch (error) {
         res.status(401).json({ message: "Token không hợp lệ" });
