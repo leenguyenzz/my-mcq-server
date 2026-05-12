c65cb1a2013faa07d6a9b3cdbbc5d4186766b951
leenguyenzz <email-cua-ban@gmail.com>
2026-05-12 12:50:04 +0700

    v

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,7 +53,7 @@ exports.login = async (req, res) => {
             res.cookie('refreshToken', refreshToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production', // true nếu dùng https
-                sameSite: 'none',
+                sameSite: 'Strict',
                 maxAge: 7 * 24 * 60 * 60 * 1000 // 7 ngày
             });
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,19 +3,12 @@ const jwt = require('jsonwebtoken');
 
 exports.protect = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
-    console.log("Token nhận được ở server:", token); // Log để kiểm tra token có được gửi lên không
     
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        console.log("Lỗi: Header Authorization trống hoặc sai định dạng Bearer");
-        return res.status(401).json({ message: "Không có quyền truy cập" });
-    }
     if (!token) return res.status(401).json({ message: "Không có quyền truy cập" });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-        console.log("Token đã được giải mã:", decoded); // Log thông tin user từ token
         next();
     } catch (error) {
         res.status(401).json({ message: "Token không hợp lệ" });
