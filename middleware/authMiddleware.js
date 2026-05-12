6fd78603c5d1973b9d29d8c1662cb31a0448f345
leenguyenzz <email-cua-ban@gmail.com>
2026-05-12 10:24:52 +0700

    d

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,6 +6,7 @@ exports.protect = (req, res, next) => {
     // 1. Log để xem headers thực tế
     console.log("Headers nhận được:", req.headers);
     // 2. Kiểm tra nếu header hoàn toàn không tồn tại
+    const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         console.log("Lỗi: Header Authorization trống hoặc sai định dạng Bearer");
         return res.status(401).json({ message: "Không có quyền truy cập" });
