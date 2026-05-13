610550bf54f88d3b2426b5af3ad1fcdd35a36aeb
leenguyenzz <email-cua-ban@gmail.com>
2026-05-13 08:26:03 +0700

    v

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -15,15 +15,15 @@ exports.deposit = (req, res) => {
         });
 }
 
-exports.withdraw = (req, res) => {
+exports.withdraw = async (req, res) => {
     const userId = req.user.id; // Giả sử bạn đã có middleware xác thực và gắn user vào req
     const { amount } = req.body;
     // Logic để xử lý rút tiền từ tài khoản
-    const balance = Bank.findOne({ userId: userId }).then(bank => bank.balance);
+    const balance = await Bank.findOne({ userId: userId }).then(bank => bank.balance);
     if (balance < amount) {
         return res.status(400).json({ error: 'Số dư không đủ để rút!' });
     }
-    Bank.findOneAndUpdate({ userId: userId }, { $inc: { balance: -amount } }, { new: true })
+    await Bank.findOneAndUpdate({ userId: userId }, { $inc: { balance: -amount } }, { new: true })
         .then(bank => {
             res.json({ message: `Đã rút ${amount} từ tài khoản!`, balance: bank.balance });
         })
@@ -32,31 +32,31 @@ exports.withdraw = (req, res) => {
         });
 }
 
-exports.transfer = (req, res) => {
+exports.transfer = async (req, res) => {
     const userId = req.user.id; // Giả sử bạn đã có middleware xác thực và gắn user vào req
     const { amount, toAccount } = req.body;
     // Logic để xử lý chuyển tiền đến tài khoản khác
-    User.findOne({ accountNumber: toAccount }).then(recipient => {
+    await User.findOne({ accountNumber: toAccount }).then(recipient => {
         if (!recipient) {
             return res.status(404).json({ error: 'Tài khoản người nhận không tồn tại!' });
         }
-        const balance = Bank.findOne({ userId: userId }).then(bank => bank.balance);
+        const balance = await Bank.findOne({ userId: userId }).then(bank => bank.balance);
         if (balance < amount) {
             return res.status(400).json({ error: 'Số dư không đủ để chuyển!' });
         }
-        return transferMoney(userId, toAccount, amount);
+        await transferMoney(userId, toAccount, amount);
     }).catch(err => {
         res.status(500).json({ error: 'Lỗi hệ thống' });
     });
 }
 
-const transferMoney = (userId, toAccount, amount) => {
+const transferMoney = async (userId, toAccount, amount) => {
     // Trừ tiền từ tài khoản người gửi và cộng tiền vào tài khoản người nhận
-    Bank.findOneAndUpdate({ userId: userId }, { $inc: { balance: -amount } }, { new: true })
-        .then(bank => {
+    await Bank.findOneAndUpdate({ userId: userId }, { $inc: { balance: -amount } }, { new: true })
+        .then(async bank => {
             // Sau khi trừ tiền từ tài khoản người gửi, cộng tiền vào tài khoản người nhận
-            Bank.findOneAndUpdate({ accountNumber: toAccount }, { $inc: { balance: amount } }, { new: true })
-                .then(bank => {
+            await Bank.findOneAndUpdate({ accountNumber: toAccount }, { $inc: { balance: amount } }, { new: true })
+                .then(async bank => {
                     res.json({ message: `Đã chuyển ${amount} đến tài khoản ${toAccount}!` });
                 })
                 .catch(err => {
@@ -69,10 +69,10 @@ const transferMoney = (userId, toAccount, amount) => {
     // res.json({ message: `Đã chuyển ${amount} đến tài khoản ${toAccount}!` }); // This line is redundant and should be removed
 }
 
-exports.getBalance = (req, res) => {
+exports.getBalance = async (req, res) => {
     const userId = req.user.id; // Giả sử bạn đã có middleware xác thực và gắn user vào req
     // Logic để lấy số dư tài khoản
-    Bank.findOne({ userId: userId }).then(bank => {
+    await Bank.findOne({ userId: userId }).then(bank => {
         if (!bank) {
             return res.status(404).json({ error: 'Tài khoản không tồn tại!' });
         }
