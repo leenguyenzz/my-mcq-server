b50ec43930a79f973338f6d51c4038f615735cfd
leenguyenzz <leenguyen.nll@gmail.com>
2026-05-14 21:39:12 +0700

    v

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(limiter);
+// app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 // Chế độ 'dev' sẽ cho ra các dòng log có màu sắc, dễ nhìn
